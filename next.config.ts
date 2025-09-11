5c3d34e113ebcda540b8d8e42287474e50101314
b-rucel <b-rucel@users.noreply.github.com>
2025-09-11 11:52:57 -0700

    first commit

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+    // Enable Cloudflare Pages compatibility
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+    experimental: {
+    }
+};
